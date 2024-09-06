a044e6cff721322edd65be8a8242ca3cfc3d1315
Miguel Dominguez <a.22308051281173@cbtis128.edu.mx>
2024-09-06 09:40:21 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,25 @@
+"""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+nombre="Miguel"
+print(nombre[0])
+print(nombre)
+MisNovias=["Agripina", "Anastacia", "Maria"]
+for chicas in MisNovias:
+  print(chicas)
+
+  print("otro ejemplo")
+  b = "Miguel Dominguez"
+print(b[0:6])
+
+print("a mayusculas")
+a = "migel dominguez"
+print(a.upper())
+
+print("concatenar")
+a = "Maria"
+b = "Perez"
+c = a +" "+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17 # 17 se guarda en edad
+nombre="Frida Cisneros"
+print("Tu edad es ",edad)
+print("Te llamas",nombre)
+print("--tipo de variables--")
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--funciones matematicas--")
+x = math.sqrt(64)
+
+print(x)
